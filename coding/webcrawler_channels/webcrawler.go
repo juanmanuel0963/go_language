3d8623aa3b573046a5d3786c6fd9f5cfac102814
juanmanuel0963 <juanmanuel0963@gmail.com>
2023-10-10 11:36:18 -0500

    commit

--- coding/webcrawler_channels/webcrawler.go
+++ coding/webcrawler_channels/webcrawler.go
@@ -6,7 +6,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -28,12 +30,15 @@ var baseDomain = ""
 var crawledPages Pages
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run webcrawler.go <starting_url>")
+
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run webcrawler.go <starting_url> <deep_level_to_retrieve>")
 		os.Exit(1)
 	}
 	startingURL := os.Args[1]
-	baseDomain = getBaseDomain(startingURL)
+	baseDomain = GetBaseDomain(startingURL)
+	maxDeepLevel, _ := strconv.ParseInt(os.Args[2], 0, 0)
+
 	crawledPages.visited = make(map[string]bool)
 
 	root := &TreeNode{Name: startingURL} // Create the root node
@@ -44,7 +49,7 @@ func main() {
 
 	wg.Add(1) // Increment the wait group count
 
-	go RecursiveCrawl(startingURL, 0, 0, doneChan, root, wg)
+	go RecursiveCrawl(startingURL, 0, int(maxDeepLevel), 0, doneChan, root, wg)
 
 	go func() {
 		wg.Wait() // Wait for all goroutines to finish
@@ -60,9 +65,20 @@ func main() {
 	PrintTree(root, 0)
 
 	// Convert the tree to JSON
-	jsonData, err := convertTreeToJson(root)
+	jsonData, err := ConvertTreeToJson(root)
 	if err != nil {
-		fmt.Printf("Error converting tree to JSON: %v\n", err)
+		errMsg := fmt.Sprintf("Domain: %s Message: %s Error: %s", baseDomain, "Error converting tree to JSON.", err)
+		LogError(errMsg)
+		return
+	}
+
+	//Get the domain in the format mydomain.com
+	domain := GetShortDomain(baseDomain)
+
+	err = SaveJSONToFileWithTimestamp(jsonData, domain)
+	if err != nil {
+		errMsg := fmt.Sprintf("Domain: %s Message: %s Error: %s", baseDomain, "Error saving JSON to file.", err)
+		LogError(errMsg)
 		return
 	}
 
@@ -70,27 +86,29 @@ func main() {
 	fmt.Printf("Tree structure in JSON:\n%s\n", jsonData)
 
 	elapsed := time.Since(startTime)
+
 	fmt.Printf("Total time taken: %s\n", elapsed)
+
 }
 
-func RecursiveCrawl(parentURL string, level int, parent int, doneChan chan string, parentNode *TreeNode, wg *sync.WaitGroup) {
+func RecursiveCrawl(parentURL string, currentLevel int, maxLevel int, parent int, doneChan chan string, parentNode *TreeNode, wg *sync.WaitGroup) {
 	defer func() {
 		doneChan <- parentURL
 	}()
 
-	//if level < 2 {
-	foundLinks := Crawl(parentURL)
-	fmt.Printf("\nLevel:%v, Parent: %v, %s, \n", level, parent, parentURL)
-	counter := 0
-	for sonURL := range foundLinks {
-		counter++
-		//fmt.Printf("Son %v: %s\n", counter, sonURL)
-		wg.Add(1) // Increment the wait group count
-		sonNode := &TreeNode{Name: sonURL}
-		parentNode.Children = append(parentNode.Children, sonNode) // Add child node to parent
-		go RecursiveCrawl(sonURL, level+1, counter, doneChan, sonNode, wg)
+	if currentLevel <= maxLevel {
+		foundLinks := Crawl(parentURL)
+		fmt.Printf("\nLevel:%v, Parent: %v, %s", currentLevel, parent, parentURL)
+		counter := 0
+		for sonURL := range foundLinks {
+			counter++
+			//fmt.Printf("Son %v: %s\n", counter, sonURL)
+			wg.Add(1) // Increment the wait group count
+			sonNode := &TreeNode{Name: sonURL}
+			parentNode.Children = append(parentNode.Children, sonNode) // Add child node to parent
+			go RecursiveCrawl(sonURL, currentLevel+1, maxLevel, counter, doneChan, sonNode, wg)
+		}
 	}
-	//}
 	wg.Done() // Decrement the parent wait group count
 }
 
@@ -110,6 +128,9 @@ func Crawl(parentURL string) map[string]bool {
 
 	if err != nil {
 		fmt.Printf("Error fetching %s: %v\n", parentURL, err)
+		errMsg := fmt.Sprintf("Domain: %s Message: %s Error: %s", baseDomain, "Error fetching "+parentURL, err)
+		LogError(errMsg)
+
 		return foundLinks
 	}
 
@@ -117,6 +138,9 @@ func Crawl(parentURL string) map[string]bool {
 
 	if response.StatusCode != http.StatusOK {
 		fmt.Printf("Error fetching %s: Status Code %d\n", parentURL, response.StatusCode)
+		errMsg := fmt.Sprintf("Domain: %s Message: %s Error: %s", baseDomain, "Error fetching "+parentURL+" Status Code "+fmt.Sprint(response.StatusCode), err)
+		LogError(errMsg)
+
 		return foundLinks
 	}
 
@@ -131,7 +155,9 @@ func Crawl(parentURL string) map[string]bool {
 			if err == io.EOF {
 				return foundLinks
 			}
-			log.Fatalf("error tokenizing HTML: %v", tokenizer.Err())
+
+			errMsg := fmt.Sprintf("Domain: %s Message: %s Error: %s", baseDomain, "Error tokenizing HTML ", tokenizer.Err())
+			LogError(errMsg)
 
 		case html.StartTagToken, html.SelfClosingTagToken:
 			token := tokenizer.Token()
@@ -156,16 +182,21 @@ func Crawl(parentURL string) map[string]bool {
 	//return foundLinks
 }
 
-func getBaseDomain(url string) string {
+func GetBaseDomain(url string) string {
 	parts := strings.Split(url, "/")
 	if len(parts) < 3 {
+
 		fmt.Println("Invalid URL")
+
+		errMsg := fmt.Sprintf("Domain: %s Message: %s Error: %s", baseDomain, "Invalid URL ", url)
+		LogError(errMsg)
+
 		os.Exit(1)
 	}
 	return parts[0] + "//" + parts[2]
 }
 
-func convertTreeToJson(root *TreeNode) ([]byte, error) {
+func ConvertTreeToJson(root *TreeNode) ([]byte, error) {
 	jsonData, err := json.Marshal(root)
 	if err != nil {
 		return nil, err
@@ -175,17 +206,104 @@ func convertTreeToJson(root *TreeNode) ([]byte, error) {
 
 // Function to recursively print the tree
 func PrintTree(node *TreeNode, level int) {
-	fmt.Printf("%s%s\n", getIndent(level), node.Name)
+	fmt.Printf("%s%s\n", GetIndent(level), node.Name)
 	for _, child := range node.Children {
 		PrintTree(child, level+1)
 	}
 }
 
 // Helper function to get the appropriate indentation
-func getIndent(level int) string {
+func GetIndent(level int) string {
 	indent := ""
 	for i := 0; i < level; i++ {
 		indent += "-"
 	}
 	return indent
+}
+
+// Saves JSON data to a file with a timestamp in the filename.
+func SaveJSONToFileWithTimestamp(jsonData []byte, prefix string) error {
+	// Create the "sitemaps" subfolder if it doesn't exist
+	sitemapsFolder := "sitemaps"
+	if _, err := os.Stat(sitemapsFolder); os.IsNotExist(err) {
+		os.Mkdir(sitemapsFolder, os.ModePerm)
+	}
+
+	// Set the format expected for output
+	timestamp := time.Now().Format("2006-01-02_15-04-05") // Format: YYYY-MM-DD_HH-MM-SS
+
+	// Create the filename within the "sitemaps" subfolder
+	filename := fmt.Sprintf("%s/%s_%s.json", sitemapsFolder, prefix, timestamp)
+
+	// Open the file on disk
+	file, err := os.Create(filename)
+	if err != nil {
+		errMsg := fmt.Sprintf("Domain: %s Message: %s Error: %s", baseDomain, "Error opening the file", filename)
+		LogError(errMsg)
+		return err
+	}
+	defer file.Close()
+
+	// Write data to the file
+	_, err = file.Write(jsonData)
+	if err != nil {
+		errMsg := fmt.Sprintf("Domain: %s Message: %s Error: %s", baseDomain, "Error writing to the file", filename)
+		LogError(errMsg)
+		return err
+	}
+
+	fmt.Printf("JSON data saved to %s\n", filename)
+	return nil
+}
+
+// Return an URL in the format mydomain_com
+func GetShortDomain(inputURL string) string {
+	// Parse the input URL
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+
+		fmt.Println("Invalid URL:", err)
+
+		errMsg := fmt.Sprintf("Domain: %s Message: %s Error: %s", baseDomain, "Invalid URL ", err)
+		LogError(errMsg)
+
+		return ""
+	}
+
+	// Remove "www." from the host if present
+	host := strings.TrimPrefix(parsedURL.Hostname(), "www.")
+
+	// Replace periods with underscores to get the base domain
+	baseDomain := strings.Replace(host, ".", "_", -1)
+
+	return baseDomain
+}
+
+func LogError(errorMessage string) {
+	// Create a "logs" subfolder if it doesn't exist
+	logsFolder := "logs"
+	if _, err := os.Stat(logsFolder); os.IsNotExist(err) {
+		os.Mkdir(logsFolder, os.ModePerm)
+	}
+
+	// Get the current date and time
+	currentTime := time.Now()
+	// Format the date in YYYY-MM-DD format
+	date := currentTime.Format("2006-01-02")
+
+	// Create or open the log file in the "logs" subfolder with the current date as the filename
+	logFileName := fmt.Sprintf("%s/%s.log", logsFolder, date)
+	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		// If there is an error opening the log file, print the error and exit
+		fmt.Printf("Error opening log file: %v\n", err)
+		return
+	}
+	defer logFile.Close()
+
+	// Create a new logger that writes to the log file
+	logger := log.New(logFile, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Log the error message with the current date and time
+	logger.Printf("%s - %s", currentTime.Format("2006-01-02 15:04:05"), errorMessage)
 }
--- go.mod
+++ go.mod
@@ -5,18 +5,21 @@ go 1.21
 require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/shomali11/gridder v0.0.0-20210930173142-5f3b82d74585
-	github.com/stretchr/testify v1.7.1
+	github.com/stretchr/testify v1.8.1
 	golang.org/x/exp v0.0.0-20220318154914-8dddf5d87bd8
 	golang.org/x/image v0.0.0-20220302094943-723b81ca9867
 )
+
+require go.uber.org/multierr v1.10.0 // indirect
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/go-playground/assert/v2 v2.2.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	go.uber.org/zap v1.26.0
 	golang.org/x/net v0.15.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
